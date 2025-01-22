6ec3e3eb6998ecebfe74135bdafd5f68887ee9f8
md073aftab <u24ai058@aid.svnit.ac.in>
2025-01-22 22:50:17 +0530

    commit

--- python and webprogramming/assignments/assignment 1/Q4.py
+++ python and webprogramming/assignments/assignment 1/Q4.py
@@ -1,3 +1,4 @@
 #Take numbers from 1 to 10000. Create equivalence classes for modulo 5 on this set of
 #numbers. Check the validity of your equivalence classes. [Hint: the union of all equivalence
 #classes should be the original set/list.]
+all_num=[for i=1 in range(10001)]
