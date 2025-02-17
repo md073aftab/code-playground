29b88134e475a95d6a6c261f7089adb263729b17
md073aftab <u24ai058@aid.svnit.ac.in>
2025-02-17 14:43:23 +0530

    commit

--- DSA/DSA_ASSIGNMENTS/assignment_5/Q3.c
+++ DSA/DSA_ASSIGNMENTS/assignment_5/Q3.c
@@ -1,31 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node{
+
+struct Node {
     int data;
-    struct node* next;
+    struct Node* next;
 };
 
-void linkedlisttraversal(struct node* ptr){
-    while(ptr!=NULL){
-        printf("%d\t",ptr->data);
-        ptr=ptr->next;
+void linkedListTraversal(struct Node* ptr) {
+    while (ptr != NULL) {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
     }
-    printf("\n");
 }
 
-int isempty(struct node* top){
-    if(top==NULL){
+int isEmpty(struct Node* top) {
+    return top == NULL;
+}
+
+int isFull(struct Node* top) {
+    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+    if (p == NULL) {
         return 1;
-    }
-    else{
+    } else {
+        free(p); // Free the allocated memory
         return 0;
     }
 }
 
-int isfull(struct node* top){
-    struct node* 
+struct Node* push(struct Node* top, int x) {
+    if (isFull(top)) {
+        printf("Stack Overflow\n");
+    } else {
+        struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+        n->data = x;
+        n->next = top;
+        top = n;
+    }
+    return top;
 }
+
+int pop(struct Node** top) {
+    if (isEmpty(*top)) {
+        printf("Stack Underflow\n");
+        return -1; // Indicating an error
+    } else {
+        struct Node* n = *top;
+        *top = (*top)->next;
+        int x = n->data;
+        free(n);
+        return x;
+    }
+}
+
 int main() {
-    
+    struct Node* top = NULL;
+    int choice, data, position;
+
+    while (1) {
+        printf("\nChoose an operation:\n");
+        printf("1: Push element\n");
+        printf("2: Pop element\n");
+        printf("3: Display stack\n");
+        printf("0: Terminate\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter data to push: ");
+                scanf("%d", &data);
+                top = push(top, data);
+                break;
+            case 2:
+                data = pop(&top);
+                if (data != -1) {
+                    printf("Popped element: %d\n", data);
+                }
+                break;
+            case 3:
+                linkedListTraversal(top);
+                break;
+            case 0:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+
     return 0;
 }
---
+++ DSA/DSA_ASSIGNMENTS/assignment_6/Q1.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 1000
+
+typedef struct stack {
+    char arr[MAX];
+    int top;
+} stack;
+
+void push(stack *s, char c) {
+    s->top++;
+    s->arr[s->top] = c;
+}
+
+char pop(stack *s) {
+    if (s->top == -1) return '\0';
+    return s->arr[s->top--];
+}
+
+char peek(stack *s) {
+    return (s->top == -1) ? '\0' : s->arr[s->top];
+}
+
+int priority(char c) {
+    if (c == '+' || c == '-') return 1;
+    if (c == '*' || c == '/') return 2;
+    if (c == '^') return 3;
+    return 0;
+}
+
+int right_associative(char c) {
+    return c == '^';
+}
+
+void reverse(char *exp) {
+    int n = strlen(exp);
+    for (int i = 0; i < n / 2; i++) {
+        char temp = exp[i];
+        exp[i] = exp[n - i - 1];
+        exp[n - i - 1] = temp;
+    }
+}
+
+void infixToPostfix(char *exp, char *res) {
+    stack Stack;
+    Stack.top = -1;
+    int i = 0;
+
+    for (int j = 0; j < strlen(exp); j++) {
+        if (isalnum(exp[j])) {
+            res[i++] = exp[j];
+        } else if (exp[j] == '(') {
+            push(&Stack, exp[j]);
+        } else if (exp[j] == ')') {
+            while (Stack.top != -1 && peek(&Stack) != '(') {
+                res[i++] = pop(&Stack);
+            }
+            pop(&Stack);
+        } else {
+            while (Stack.top != -1 && (priority(peek(&Stack)) > priority(exp[j]) || 
+                (priority(peek(&Stack)) == priority(exp[j]) && !right_associative(exp[j])))) {
+                res[i++] = pop(&Stack);
+            }
+            push(&Stack, exp[j]);
+        }
+    }
+    while (Stack.top != -1) {
+        res[i++] = pop(&Stack);
+    }
+    res[i] = '\0';
+}
+
+void infixToPrefix(char *exp, char *res) {
+    reverse(exp);
+    for (int i = 0; i < strlen(exp); i++) {
+        if (exp[i] == '(') exp[i] = ')';
+        else if (exp[i] == ')') exp[i] = '(';
+    }
+    infixToPostfix(exp, res);
+    reverse(res);
+}
+
+int main() {
+    char exp[MAX], res[MAX];
+    int choice;
+
+    do {
+        printf("Enter infix expression: ");
+        fgets(exp, MAX, stdin);
+        exp[strcspn(exp, "\n")] = '\0';
+
+        printf("Choose an option:\n1. Infix to Postfix\n2. Infix to Prefix\n3. Quit\nEnter choice: ");
+        scanf("%d", &choice);
+        getchar();
+
+        switch (choice) {
+            case 1:
+                infixToPostfix(exp, res);
+                printf("Postfix Expression: %s\n", res);
+                break;
+            case 2:
+                infixToPrefix(exp, res);
+                printf("Prefix Expression: %s\n", res);
+                break;
+            case 3:
+                printf("Exiting program...\n");
+                break;
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    } while (choice != 3);
+    
+    return 0;
+}
