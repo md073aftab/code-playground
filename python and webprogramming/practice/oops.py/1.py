61678a2360bea66b908f41c7c633f9ec1fec0bda
md073aftab <u24ai058@aid.svnit.ac.in>
2025-03-05 22:31:40 +0530

    cc

--- python and webprogramming/practice/oops.py/1.py
+++ python and webprogramming/practice/oops.py/1.py
@@ -1,14 +1,29 @@
-class student:
-    class_year=2025
-    num_student=0
+class password_manager:
 
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-        student.num_student+=1
+    def __init__(self):
+        self.old_passwords=old_passwords=[]
+        self.current_password="0000"
 
-student1=student("aftab",19)
-student2=student("karan",18)
-student3=student("keval",19)
+    def set_password(self,new_password):
+        if new_password not in self.old_passwords:
+            self.current_password=new_password
+            self.old_passwords.append(self.current_password)
+        else:
+            print("This password already exist, try another password")
+    def get_password(self):
+        curr=self.old_passwords[-1]
+        print(f"current password is {curr}")
 
-print(student1.name)
+    def is_correct(self,password):
+        if password==self.old_passwords[-1]:
+            print("**CORRECT PASSWORD**")
+        else :
+            print("**password incorrect**")
+
+app1=password_manager()
+app1.set_password("Md073@ftab")
+app1.get_password()
+
+
+
+        
--- python and webprogramming/practice/oops.py/2.py
+++ python and webprogramming/practice/oops.py/2.py
@@ -1,21 +1,34 @@
-class animal:
+import random
 
-    def __init__(self,name):
-        self.name=name
-    def eat(self):
-        print(f"{self.name} is eating")
-
-class prey(animal):
-    def flee(self):
-        print(f"{self.name} is fleeing")
-class predator(animal):
-    def hunt(self):
-        print(f"{self.name }is hunting")
-class rabbit(prey):
-    def eat(se):
-        print(f"{se.name} eats grass")
-
-abbit=rabbit("sim")     
-rrabbit=prey("sim")     
-rrabbit.eat()
- 
+class Rock_paper_scissors():
+    def __init__(self):
+        choices=["stone","paper","scissor"]
+        user_wins=0
+    def rounds(self):
+        rounds=int(input("HELLO, how many rounds wound like to play"))
+        return rounds
+    def user_choice(self):
+        self.user_choice=input("enter user choice from: {stone,paper,scissor}").lower()
+        #while self.user_choice not in self.choices:
+           # print("invalid choice")
+           # self.user_choice=input("enter user choice from: {stone,paper,scissor}").lower()
+        return self.user_choice    
+    def computer_choice(self): 
+        return random.choice(self.choices)
+    def round_winner(self,user_choice):
+        self.user_choice=user_choice
+        self.comp_choice=self.computer_choice()
+        if user_choice=="stone" and self.comp_choice=="scissor" or user_choice=="scissor" and self.comp_choice=="paper" or user_choice=="paper" and self.comp_choice=="stone":
+            self.user_wins += 1
+            print("user WON!!")
+        else:
+            print("computer WON!!")                        
+    def winner(self,user_wins):
+        if self.rounds/2 < self.user_wins:
+            print("USER WON!!")
+        else:
+            print("COMPUTER WON!!")
+user1=Rock_paper_scissors()
+for i in range(0,user1.rounds()) :
+    user1.round_winner(user1.user_choice())
+user1.winner()
